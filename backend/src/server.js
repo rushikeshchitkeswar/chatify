eebc2f5c5ab068e573dac7f21c20939de9a40064
rushikesh <chitkeshwarr@gmail.com>
2025-10-08 01:06:01 +0530

    change some code

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,6 @@ app.use(express.json({ limit: "5mb" })); // req.body
 app.use(
   cors({
     origin: "*",
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
