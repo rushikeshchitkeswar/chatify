c1bdc839200cca81f7c92e6cd72d1b561c3b4b50
rushikesh <chitkeshwarr@gmail.com>
2025-10-08 01:15:58 +0530

    final changes to file

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,6 +23,7 @@ app.use(
       "http://localhost:5173", // for development
       "https://coruscating-gumdrop-03f53f.netlify.app", // for production
     ],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
